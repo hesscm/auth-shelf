0895931ee1e02744ecda0baf49194d52acf2b601
Chris Hess <65086478+hesscm@users.noreply.github.com>
2021-10-08 09:50:15 -0500

    added useEffect

--- src/components/ShelfPage/ShelfList.jsx
+++ src/components/ShelfPage/ShelfList.jsx
@@ -2,15 +2,25 @@ import ShelfListItem from "./ShelfListItem";
 import useReduxStore from "../../hooks/useReduxStore";
 import "./ShelfPage.css"
 import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 function ShelfList() {
     const store = useReduxStore();
-
     const tempArray = [
         { id: 1, description: 'Star Wars', image_url: 'https://m.media-amazon.com/images/M/MV5BMDljNTQ5ODItZmQwMy00M2ExLTljOTQtZTVjNGE2NTg0NGIxXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_FMjpg_UX1000_.jpg'},
         { id: 1, description: 'Star Wars', image_url: 'https://m.media-amazon.com/images/M/MV5BMDljNTQ5ODItZmQwMy00M2ExLTljOTQtZTVjNGE2NTg0NGIxXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_FMjpg_UX1000_.jpg' },
-
 ];
+
+const fetchList = () => {
+    dispatchEvent({type: "FETCH_LIST"})
+}
+
+
+useEffect(() => {
+    fetchList();
+
+}, [])
+
 
     return (
         <div>
